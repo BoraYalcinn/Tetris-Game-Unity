7f260df98208debdbfe4bced06bc09b549468f96
BoraYalcinn <borayalcintr@gmail.com>
2026-01-20 22:36:11 +0300

    wall kick logic is implemented 'SRS'

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -62,6 +62,19 @@ public class Piece : MonoBehaviour
 
         private void Rotate(int direction)
         {
+                int originalRotation = this.rotationIndex;
+                this.rotationIndex = Wrap(this.rotationIndex+direction,0,4);
+                ApplyRotationMatrix(direction);
+
+                if (!TestWallKicks(this.rotationIndex,direction))
+                {
+                        this.rotationIndex = originalRotation;
+                        ApplyRotationMatrix(-direction);
+                }
+        }
+        
+        private void ApplyRotationMatrix(int direction)
+        {
                 this.rotationIndex += Wrap(this.rotationIndex+direction,0,4);
                 for (int i = 0; i < this.cells.Length; i++)
                 {
@@ -76,20 +89,21 @@ public class Piece : MonoBehaviour
                                         cell.x -= 0.5f;
                                         cell.y -= 0.5f;
                                         x = Mathf.CeilToInt((cell.x * Data.RotationMatrix[0] * direction) +
-                                                             (cell.y * Data.RotationMatrix[1] * direction));
+                                                            (cell.y * Data.RotationMatrix[1] * direction));
                                         y = Mathf.CeilToInt((cell.x * Data.RotationMatrix[2] * direction) +
-                                                             (cell.y * Data.RotationMatrix[3] * direction));
+                                                            (cell.y * Data.RotationMatrix[3] * direction));
                                         
                                         break;
                                 default:
                                         x = Mathf.RoundToInt((cell.x * Data.RotationMatrix[0] * direction) +
-                                                (cell.y * Data.RotationMatrix[1] * direction));
+                                                             (cell.y * Data.RotationMatrix[1] * direction));
                                         y = Mathf.RoundToInt((cell.x * Data.RotationMatrix[2] * direction) +
-                                                (cell.y * Data.RotationMatrix[3] * direction));
+                                                             (cell.y * Data.RotationMatrix[3] * direction));
                                         break;
                         }
                         this.cells[i] = new Vector3Int(x,y,0);
                 }
+                
         }
 
         private void HardDrop()
@@ -115,6 +129,34 @@ public class Piece : MonoBehaviour
 
                 return valid;
         }
+
+        private bool TestWallKicks(int rotationIndex, int rotationDirection)
+        {
+                int wallKickIndex = GetWallKickIndex(rotationIndex,rotationDirection);
+
+                for (int i = 0; i < wallKickIndex; i++)
+                {
+                        Vector2Int translation = this.data.wallKicks[wallKickIndex,i];
+                        if (Move(translation))
+                        {
+                                return true;
+                        }
+                }
+
+                return false;
+        }
+
+        private int GetWallKickIndex(int rotationIndex, int rotationDirection)
+        {
+                int wallKickIndex = rotationIndex * 2;
+
+                if (rotationDirection < 0)
+                {
+                        wallKickIndex--;
+                }
+                return Wrap(wallKickIndex,0,this.data.wallKicks.GetLength(0));
+        }
+        
 
         private int Wrap(int input, int min, int max)
         {
